288d70e2806756fda8720d07a9346c212b1efe63
AuserZ <mzainurroziqinakbar@gmail.com>
2024-09-29 01:52:18 +0700

    add logs

--- src/app/api/v1/dashboard/route.tsx
+++ src/app/api/v1/dashboard/route.tsx
@@ -1,45 +1,33 @@
 import { NextRequest, NextResponse } from 'next/server';
 import pool from '@/lib/db';
-import { z } from 'zod';
 import dotenv from 'dotenv';
 import { QUERY_DASHBOARD } from '@/lib/Query';
 
 dotenv.config();
 
-// Define a schema for input validation using Zod
-// const bodyRequest = z.object({
-//   productName: z.string().optional(),
-//   productPrice: z.number().optional(),
-//   totalItems: z.number().optional(),
-//   soldCount: z.number().optional(),
-//   warehouseLocID: z.string().optional(),
-//   vendorID: z.string().optional(),
-// });
-
-export async function POST(request: NextRequest): Promise<NextResponse> {
+export async function GET(request: NextRequest): Promise<NextResponse> {
   // Basic authorization check
   const apiKey = request.headers.get('x-api-key');
   if (apiKey !== process.env.X_API_KEY) {
     return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
   }
 
-  // Parse and validate request body
-  // const body = await request.json();
-  // const validationResult = bodyRequest.safeParse(body);
-  // if (!validationResult.success) {
-  //   return NextResponse.json({ error: 'Invalid request body' }, { status: 400 });
-  // }
-
-  // const { productName, productPrice, totalItems, soldCount, warehouseLocID, vendorID } = validationResult.data;
-
   let conn;
   try {
+    console.log('start - Dashboard GET');
     conn = await pool.getConnection();
+    
+    console.log('connection established');
 
     let query = QUERY_DASHBOARD;
+    console.log('query:', query);
 
     const rows = await conn.query(query);
-    return NextResponse.json(rows);
+    const response = NextResponse.json(rows);
+
+    console.log('Response', response);
+    console.log('end - Dashboard GET');
+    return response;
   } catch (err) {
     console.error(err);
     return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
